ac2cbe3bb2bf7d24f3076302990899d18072bedf
Keyonna Mosley <kenmosley91@gmail.com>
2024-09-12 22:30:34 -0400

    started working on request a qoute page

---
+++ quote.js
@@ -0,0 +1,18 @@
+const hiThere = document.getElementById('hello')
+hiThere.style.display = 'none'
+const residential = document.getElementById('residential')
+const commercial = document.getElementById('commercial')
+const industrial = document.getElementById('industrial')
+const standard = document.getElementById('standard')
+const premium = document.getElementById('premium')
+const excelium = document.getElementById('excelium')
+const numberOfApartments = document.getElementById('number-of-apartments')
+const numberOfFloors = document.getElementById('number-of-floors')
+const numberOfElevators = document.getElementById('number-of-elevators')
+const maximumOccupancy = document.getElementById('maximum-occupancy')
+
+
+const apartmentsInput = document.getElementById('apartments-input')
+const floorsInput = document.getElementById('floors-input')
+const elevatorsInput = document.getElementById('elevators-input')
+const occupancyInput = document. getElementById('occupancy-input')
